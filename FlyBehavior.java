c3bb68ec22e9d506f8b01910654fb759440e4515
Trần Văn Hiến <hientv_55@vnu.edu.vn>
2016-05-25 10:40:40 +0700

    ab
    cd

---
+++ FlyBehavior.java
@@ -0,0 +1,5 @@
+package train;
+
+public interface FlyBehavior {
+	public  void fly();
+}
---
+++ FlyRocketPowered.java
@@ -0,0 +1,7 @@
+package train;
+
+public class FlyRocketPowered implements FlyBehavior{
+	public void fly(){
+		System.out.println("I'm flying with a rocket!");
+	}
+}
